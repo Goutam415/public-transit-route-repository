fdbbabc9eb35bacca2f53d52c9231416e061f57b
Goutam <goutam415@gmail.com>
2022-02-28 08:34:11 +0530

    Fix linting issue

--- api/models/route-stop.js
+++ api/models/route-stop.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 const routeStopSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true },
-    description: { type: String, required: true },
-    stopId: { 
+    name: {
+        type: String,
+        required: true
+    },
+    lat: {
+        type: Number,
+        required: true
+    },
+    lng: {
+        type: Number,
+        required: true
+    },
+    stopId: {
         type: String,
         require: true,
         unique: true,
--- api/models/user-route.js
+++ api/models/user-route.js
@@ -2,24 +2,31 @@ const mongoose = require('mongoose');
 
 const userRouteSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    direction: { 
-        type: String, enum: ['UP', 'DOWN'],
+    name: {
+        type: String,
         required: true
     },
-    routeId: { 
+    direction: {
+        type: String,
+        enum: ['UP', 'DOWN'],
+        required: true
+    },
+    routeId: {
         type: String,
         require: true,
         unique: true,
         default: Math.random().toString(16).slice(2)
     },
-    status: { 
+    status: {
         type: String,
         default: 'Active',
         enum: ['Active', 'Inactive'],
-        require: true 
+        require: true
     },
-    stops: [{ type: mongoose.Schema.Types.ObjectId, ref: 'RouteStop' }],
+    stops: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'RouteStop'
+    }],
 });
 
 module.exports = mongoose.model('UserRoute', userRouteSchema);
